cc0075af0ce1387c0c69a0b10078524bac2fe26c
Ananya Singla <ananya.p.singla@gmail.com>
2021-12-14 00:54:59 +0530

    changes made

--- FlaskMarket/app1.py
+++ FlaskMarket/app1.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import Flask, render_template, request,jsonify
 import mysql.connector  
-from flask_mysqldb import MySQL  
+from flask_sqlalchemy import SQLAlchemy   
 import requests
 import pickle
 
@@ -10,40 +10,45 @@ from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 model = pickle.load(open('cat_model_pickle.pkl', 'rb'))
 
-con = mysql.connector.connect(host="127.0.0.1", user="root", password="Ananya162", database="employee_database")
-cursor = con.cursor()
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql + pymysql://root:Ananya162@localhost/employee_database'
+# old method 
+mydb = mysql.connector.connect(host="localhost", user="root", password="Ananya162",database="employee_database")
+mycur = mydb.cursor()
+
+
 
 @app.route('/',methods= ['GET'])
 def man():
     return render_template('predict.html')
 
-# no errors till here 
 
+# no errors till here 
 @app.route('/predict', methods=['POST'])
 def predict():
 
     if request.method == 'POST':
 
-        Employee_ID = request.args['Employee ID']
-        Date_of_Joining = datetime(request.form['Date of Joining'])
+        Employee_ID = request.args('Employee ID')
+        Date_of_Joining = request.form('Date of Joining')
 
-        Gender = request.form['Gender']       
+        Gender = request.form('Gender')       
 
-        Company_Type = request.form['Company Type']    
+        Company_Type = request.form('Company Type')    
 
-        WFH_Setup_Available = request.form['WFH Setup Available']    
+        WFH_Setup_Available = request.form('WFH Setup Available')  
 
-        Designation = float(request.form['Designation'])
+        Designation = request.form('Designation')
         
-        Working_Hours = float(request.form['Resource Allocation'])
+        Working_Hours = request.form('Resource Allocation')
       
-        Mental_Fatigue_Score = float(request.form['Mental Fatigue Score'])
+        Mental_Fatigue_Score = request.form('Mental Fatigue Score')
      
-        cursor.execute("""INSERT INTO 'Employee'('Employee_ID','Date_of_Joining','Gender','Company_Type','WFH_Setup_Available','Designation','Resource_Allocation','Mental_Fatigue_Score') VALUES('{}','{}','{}','{}','{}','{}','{}','{}')""".format(Employee_ID, Date_of_Joining, Gender, Company_Type,WFH_Setup_Available, Designation, Working_Hours, Mental_Fatigue_Score))
-        con.commit()
+        mycur.execute("""INSERT INTO 'Employee'(`Employee_ID`,`Date_of_Joining`,`Gender`,`Company_Type`,`WFH_Setup_Available`,`Designation`,`Resource_Allocation`,`Mental_Fatigue_Score`) VALUES('{}','{}','{}','{}','{}','{}','{}','{}')""".format(Employee_ID, Date_of_Joining, Gender, Company_Type,WFH_Setup_Available, Designation, Working_Hours, Mental_Fatigue_Score))
+        mycur.commit()
         #pred = model.predict([[ Employee_ID, Date_of_Joining, Gender, Company_Type,WFH_Setup_Available, Designation, Working_Hours, Mental_Fatigue_Score]])
         #output = round(pred[0],2)
-        return cursor.execute("""SELECT 'Burn_Rate' FROM `Employee` WHERE `Employee_ID` LIKE '{}'""".format(Employee_ID))
+        return mycur.execute("""SELECT 'Burn_Rate' FROM `Employee` WHERE `Employee_ID` LIKE '{}'""".format(Employee_ID))
     
     return render_template('index.html')
 
--- database.sql
+++ database.sql
@@ -1,1 +1,11 @@
-SELECT * FROM employee;
+create table employee(
+Employee_ID varchar(20) NOT NULL,
+Date_Of_Joining date NOT NULL,
+Gender varchar(10) NOT NULL,
+Company_Type varchar(10) NOT NULL,
+WFH_Setup_Available varchar(5) NOT NULL,
+Designation float NOT NULL,
+Resource_Allocation float NOT NULL,
+Mental_Fatigue_Score float NOT NULL,
+Burn_Rate float NOT NULL
+); 
